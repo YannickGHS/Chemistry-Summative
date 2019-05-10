fdc9c2acc0758cf13e1ef9afd738e0273d0cfd2d
YannickGHS <39533108+YannickGHS@users.noreply.github.com>
2019-05-10 14:49:45 -0400

    Add files via upload

--- molecule.class.js
+++ molecule.class.js
@@ -3,7 +3,7 @@ class Molecule{
         this.name = name;
         this.formula;
         this.type = 'undefined'; //type can be organic, big, small
-        this.forces = 'none'; //
+        this.forces = 'none'; //intermolecular forces
     }
     
 }
