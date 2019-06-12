ad6bfc335cad3c47e746d759086b39f4c3d921d0
YannickGHS <39533108+YannickGHS@users.noreply.github.com>
2019-06-12 17:44:28 -0400

    Add files via upload

---
+++ pages/electron-config/electron-config.data.js
@@ -0,0 +1,12 @@
+/* Chemistry Js elecron configuration viewer
+ * Filename: electron-config.data.js
+ * Author: Yannick Abouem
+ * Date: 06/10/2019
+ * ==========================================================
+ * Description
+ * ==========================================================
+ * This program given the atomic number of an element will
+ * display the electron configuration of the element.
+ */
+
+ 
